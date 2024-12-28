349803ae22f9b00ee6f102e5a86ca4b69bc0f727
David Gallego Ruiz <davidgr127@gmail.com>
2024-12-28 11:42:02 -0500

    Quinto commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,17 +4,19 @@ import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      {/*       <Container>
-        {/* <Row>
-        <div className={styles.footer_legend}>
-          <div className={styles.footer1}>footer1</div>
-          <div className={styles.footer1}>footer2</div>
-        </div>
-        {/* </Row>
-      </Container> 
-      */}
-    </div>
+    <Row>
+      <div className={styles.footer}>
+        {/*       <Container>
+          {/* <Row>
+          <div className={styles.footer_legend}>
+            <div className={styles.footer1}>footer1</div>
+            <div className={styles.footer1}>footer2</div>
+          </div>
+          {/* </Row>
+        </Container> 
+        */}
+      </div>
+    </Row>
   );
 };
 export default Footer;
